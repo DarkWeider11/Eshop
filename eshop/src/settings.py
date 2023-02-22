6812e8122c5b99860202ba0fb710fcc7d41a54bb
gabriel <gaby.bunescu@gmail.com>
2023-02-22 20:10:01 +0200

    vercel ++

--- eshop/src/settings.py
+++ eshop/src/settings.py
@@ -112,10 +112,10 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 AUTH_USER_MODEL = 'users.Users'
