477333061cf2793395f25986fb8b6b0096a2f70b
Bodrug Mihail <bodrugm@yandex.ru>
2023-02-23 12:58:33 +0200

    main: add new app payments

--- eshop/src/urls.py
+++ eshop/src/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
         path('products/', include('products.urls')),
         path('cart/', include('cart.urls')),
         path('authen/', include('authen.urls')),
-    
-        
+        path('payment/', include('payments.urls'))
         ]))
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
