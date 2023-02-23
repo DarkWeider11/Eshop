8c6272a97f74503499afbcf4049ab5370e7aeaf1
DarkWeider11 <boreagolis@gmail.com>
2023-02-23 12:13:09 +0200

    database sqllite

--- eshop/src/settings.py
+++ eshop/src/settings.py
@@ -112,14 +112,10 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'KePVBOId4o1c9kK2wJA2',
-        'HOST': 'containers-us-west-83.railway.app',
-        'PORT': '6507',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 AUTH_USER_MODEL = 'users.Users'
