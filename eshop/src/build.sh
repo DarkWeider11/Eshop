29bcfe99c9130c6d9d73b7e19bde1568f61e85b0
gabriel <gaby.bunescu@gmail.com>
2023-02-23 15:01:02 +0200

    main: update settings file, change path to vercel.json

---
+++ eshop/src/build.sh
@@ -0,0 +1,14 @@
+# Install Django
+echo "Installing Django..."
+python3.9 -m pip install Django
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
