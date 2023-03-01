51cffa2e5c5265c6f88225a1224a326fde8ed5d0
DarkWeider11 <boreagolis@gmail.com>
2023-03-01 10:06:45 +0200

    login with username

--- eshop/authen/views.py
+++ eshop/authen/views.py
@@ -131,5 +131,14 @@ def change_password_view(request):
         user.save(update_fields=['password'])
         msg = {'Password was modified successfully'}
         return response.Response(msg, status=status.HTTP_200_OK)
+    
+    user_token = models.UserToken.objects.get(
+    user_id=request.user.pk,
+    access_token=request.auth
+    )
+    user_token.access_token = None
+    user_token.refresh_token = None
+    user_token.logout_time = datetime.now()
+    user_token.save()
 
     return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
